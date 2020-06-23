8e51d830549fc69ff73a83744ca81c80b95ccbae
Mario Martin <mediabytes@icloud.com>
2020-06-23 11:46:18 -0400

    Add function invocation

--- src/tauro.ts
+++ src/tauro.ts
@@ -7,15 +7,17 @@ function main() {
         low_channel: 1,
         rate: 5000,
         samples_per_channel: 1
-    }
+    };
     const Daq: DaqDevice = new DaqDevice(serial_number);
 
     Daq.initialize(configuration);
 
-    Daq.on('event', handle_event)
+    Daq.on('event', handle_event);
 }
 
 function handle_event(sensor_name: string, data: object) {
     console.log(sensor_name);
-    console.log(data)
+    console.log(data);
 }
+
+main();
