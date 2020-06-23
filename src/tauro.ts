a4ff4d33706c04d9043a09a7160090c8edc5fdc0
Mario Martin <mediabytes@icloud.com>
2020-06-23 11:35:51 -0400

    Add execution context to javascript bridge

--- src/tauro.ts
+++ src/tauro.ts
@@ -1,3 +1,21 @@
 import * as DaqDevice from '../build/Release/mccdaq';
 
-DaqDevice;
+function main() {
+    const serial_number : string = 'L823763672';
+    const configuration = {
+        high_channel: 16,
+        low_channel: 1,
+        rate: 5000,
+        samples_per_channel: 1
+    }
+    const Daq: DaqDevice = new DaqDevice(serial_number);
+
+    Daq.initialize(configuration);
+
+    Daq.on('event', handle_event)
+}
+
+function handle_event(sensor_name: string, data: object) {
+    console.log(sensor_name);
+    console.log(data)
+}
